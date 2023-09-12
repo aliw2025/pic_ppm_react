c2ba15fa6e1b9dc660cd801753fa6100664d4150
waseem ali <aliw2025@gmail.com>
2023-09-12 11:54:16 +0500

    navigation added

---
+++ src/app/about/page.js
@@ -0,0 +1,19 @@
+'use client';
+
+import React, { useState } from 'react';
+
+
+export default function About() {
+  
+  
+  return (
+    <div className='bg-light text-dark '>
+      <div className='row'>
+       <link href='/'></link>
+        <div className='col-4'>this is about </div>
+      </div>
+     
+    </div>
+   
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,32 @@
-'use client';
-import Image from 'next/image'
-import styles from './page.module.css'
-import React, { useState } from 'react';
+  'use client';
+  import Image from 'next/image'
+  import styles from './page.module.css'
+  import React, { useState } from 'react';
+  import Link from 'next/link'; // Import the Link component
 
 
-export default function Home() {
-  const [count, setCount] = useState(0);
-  var c = 0;
-  function Click(){
-    console.log("i am clicked");
-    setCount(count+1);
-    c = c+1;
-    // count = 
-  }
-  return (
-    <div className=''>
-      <div className='row'>
-        <div className='col-4'>Count: {count} </div>
-        <div className='col-4'>c : {c}</div>
-        <div className='col-4'>col 3 </div>
-        <div className='col-4'>col 4 </div>
-        <div className='col-4'><button onClick={Click}>Click Me</button>
- </div>
+  export default function Home() {
+    const [count, setCount] = useState(0);
+    var c = 0;
+    function Click(){
+      console.log("i am clicked");
+      setCount(count+1);
+      c = c+1;
+      // count = 
+    }
+    return (
+      <div className='bg-light text-dark '>
+        <div className='row'>
+          <div className='col-4'>Count: {count} </div>
+          <div className='col-4'>c : {c}</div>
+          <div className='col-4'>col 3 </div>
+          <div className='col-4'>col 4 </div>
+         
+          <div className='col-4'><button onClick={Click}>Click Me</button>
+  </div>
+        </div>
+      
       </div>
-     
-    </div>
-    // <main className={styles.main}>
-      
-      
-    // </main>
-  )
-}
+    
+    )
+  }
